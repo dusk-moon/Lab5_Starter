3018b5d08136698f76a1e9675231fdce03b0bdd0
Henry Feng <henryfeng21@gmail.com>
2022-10-29 20:11:26 -0700

    modified js files

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -3,5 +3,43 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
+  const synth = window.speechSynthesis;
+  const voiceSelect = document.querySelector('select');
+  let voices = [];
+
+  function populateVoices() {
+    voices = synth.getVoices();
+    for (let i = 0; i < voices.length; i++) {
+      const option = document.createElement('option');
+      option.textContent = `${voices[i].name} (${voices[i].lang})`;
+      option.setAttribute('name', voices[i].name);
+      option.setAttribute('lang', voices[i].lang);
+      voiceSelect.appendChild(option);
+    }
+  }
+  populateVoices();
+  speechSynthesis.onvoiceschanged = populateVoices;
+
+  const button = document.querySelector('button');
+  const input = document.querySelector('textarea');
+  const img = document.querySelector('img');
+
+  button.addEventListener('click', (event)=>{
+    const utterThis = new SpeechSynthesisUtterance(input.value);
+    const selectedOption = voiceSelect.selectedOptions[0].getAttribute('name');
+
+    for (let i = 0; i < voices.length; i++) {
+      if (voices[i].name === selectedOption) utterThis.voice = voices[i];
+    }
+
+    synth.speak(utterThis);
+    
+    utterThis.addEventListener('start', (event)=>{
+      img.src = "assets/images/smiling-open.png";
+    })
+
+    utterThis.addEventListener('end', (event)=>{
+      img.src = "assets/images/smiling.png";
+    })
+  })
 }
--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -3,5 +3,42 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
+  const confetti = new JSConfetti();
+  const audio = document.querySelector('audio');
+  audio.controls = true;
+
+  const hornSelect = document.getElementById('horn-select');
+  const img1 = document.querySelectorAll('img')[0];
+  hornSelect.addEventListener('change', (event)=>{
+    if (hornSelect.value == 'air-horn') {
+      img1.src = 'assets/images/air-horn.svg';
+      audio.src = 'assets/audio/air-horn.mp3';
+    }
+    if (hornSelect.value == 'car-horn') {
+      img1.src = 'assets/images/car-horn.svg';
+      audio.src = 'assets/audio/car-horn.mp3';
+    }
+    if (hornSelect.value == 'party-horn') {
+      img1.src = 'assets/images/party-horn.svg';
+      audio.src = 'assets/audio/party-horn.mp3';
+    }
+  })
+
+  const volumeControl = document.querySelector('input');
+  const img2 = document.querySelectorAll('img')[1];
+  volumeControl.addEventListener('input', (event)=>{
+    audio.volume = volumeControl.value / 100;
+    if (volumeControl.value == 0) img2.src = 'assets/icons/volume-level-0.svg';
+    else if (volumeControl.value < 33) img2.src = 'assets/icons/volume-level-1.svg';
+    else if (volumeControl.value < 67) img2.src = 'assets/icons/volume-level-2.svg';
+    else img2.src = 'assets/icons/volume-level-3.svg';
+  })
+
+  const button = document.querySelector('button');
+  button.addEventListener('click', (event)=>{
+    if (hornSelect.value != 'select') {
+      if (hornSelect.value == 'party-horn') confetti.addConfetti();
+      audio.play();
+    }
+  })
 }
